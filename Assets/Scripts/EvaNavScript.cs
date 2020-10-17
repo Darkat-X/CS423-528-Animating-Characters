85bb030343e1f7460c2ee348095570f5c4e91c26
Bohui Xia <bohuixia@BohuideMacBook-Pro.local>
2020-10-16 23:17:32 -0400

    1

---
+++ Assets/Scripts/EvaNavScript.cs
@@ -0,0 +1,46 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class EvaNavScript : MonoBehaviour
+{
+    Animator anim;
+    public Transform target;
+
+
+    void Start ()
+    {
+        anim = GetComponent<Animator>();
+    }
+
+
+    void Update ()
+    {
+        var direction = transform.forward;
+        var desiredDirection = target.position - transform.position;
+        desiredDirection.y = 0;
+        var distance = desiredDirection.magnitude;
+        desiredDirection.Normalize();
+        var angularDeviation = Vector3.SignedAngle(direction, desiredDirection, Vector3.up);
+
+        var angDesiredSpeed = 0.5f;
+        var forDesiredSpeed = 0.5f;
+
+        float x_vel = 0;
+        float y_vel = 0;
+
+        if (Mathf.Abs(angularDeviation) < 45)
+        {
+            y_vel = Mathf.Min(forDesiredSpeed, distance);
+        }
+        if (Mathf.Abs(angularDeviation) > 5)
+        {
+            x_vel = Mathf.Min(Mathf.Abs(angularDeviation / 180f), angDesiredSpeed) * Mathf.Sign(angularDeviation);
+        }
+        
+
+        anim.SetFloat ("x_vel",x_vel);
+        anim.SetFloat ("y_vel",y_vel);
+    }
+}
+
---
+++ Assets/Scripts/EvaScript.cs
@@ -0,0 +1,48 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class EvaScript : MonoBehaviour
+{
+    Animator anim;
+    public float RunParam = 0;
+    public float z_vel;
+    public float timeScale;
+
+    void Start ()
+    {
+        anim = GetComponent<Animator>();
+    }
+
+
+
+    void Update ()
+    {
+        Time.timeScale = timeScale;
+
+        float x_vel = Input.GetAxis("Horizontal");
+        float y_vel = Input.GetAxis("Vertical");
+        if(Input.GetKey("left shift"))
+        {
+            RunParam = Mathf.Clamp01(RunParam + 1 * Time.deltaTime);
+        }
+        else
+        {
+            RunParam = Mathf.Clamp01(RunParam - 1 * Time.deltaTime);
+        }
+        if(Input.GetKeyDown("space"))
+        {
+            anim.SetTrigger("Jump");
+        }
+
+
+
+        anim.SetFloat ("x_vel",x_vel);
+        anim.SetFloat ("y_vel",y_vel);
+        anim.SetFloat ("Run",RunParam);
+    }
+
+}
+
+
+
